8898880aba67a8ea601163fd79d879864b7718c3
John Hoover <defvayne23@gmail.com>
2011-01-12 18:13:44 -0600

    Add timezone and date/time settings.

--- app/config.php
+++ app/config.php
@@ -1,7 +1,15 @@
 <?php
+### OPTIONS ###################################
+$aConfig["options"]["timezone"] = "America/Chicago";
+$aConfig["options"]["formatDate"] = "F d, Y";
+$aConfig["options"]["formatTime"] = "h:i a";
+###############################################
+
+### DATABASE ##################################
 $aConfig["database"]["connect"] = true;
 $aConfig["database"]["host"] = "localhost";
 $aConfig["database"]["username"] = "defvayne23";
 $aConfig["database"]["password"] = "edit13";
 $aConfig["database"]["database"] = "cms";
 $aConfig["database"]["fetch"] = "assoc"; // object, assoc, ordered
+###############################################
