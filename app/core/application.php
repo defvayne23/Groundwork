bf154b955667d894b1e060b343cba81080c312e8
John Hoover <defvayne23@gmail.com>
2012-01-12 23:03:57 -0600

    Change application so it doesn't try to auto load the model.

--- app/core/application.php
+++ app/core/application.php
@@ -11,8 +11,9 @@ class Application {
 	public $url;
 	public $param;
 	
-	public static function getInstance($sLoad = true) {
+	public static function getInstance() {
 		static $instance;
+		
 		$class = __CLASS__;
 		
 		if ( ! $instance instanceof $class) {
@@ -22,38 +23,18 @@ class Application {
 		return $instance;
 	}
 	
-	public function __construct($sLoad = true) {
-		global $sSiteRoot, $sController, $sAction, $aURL, $aURLVars, $oDatabase;
+	public function __construct() {
+		global $aConfig, $sSiteRoot, $sController, $sAction, $aURL, $aURLVars, $oDatabase;
 		
 		// Methods
-		if($sLoad === true) { // Keeps from creating an infinate loop when Load extends Application
-			$this->load = new GW_Load($this->root);
-		}
 		$this->db = $oDatabase;
 		
 		// Variables
+		$this->config = $aConfig;
 		$this->root = $sSiteRoot;
 		$this->controller = $sController;
 		$this->action = $sAction;
 		$this->url = $aURL;
 		$this->param = $aURLVars;
-	}
-	
-	public function error($sError = 404) {
-		switch($sError) {
-			case 403:
-				header('HTTP/1.1 403 Forbidden');
-				$this->load->view('error/403.php');
-				break;
-			case 404:
-				header('HTTP/1.1 404 Not Found');
-				$this->load->view('error/404.php');
-				break;
-			case 500:
-				header('HTTP/1.1 500 Internal Server Error');
-				$this->load->view('error/500.php');
-				break;
-		}
-		exit;
 	}
 }
